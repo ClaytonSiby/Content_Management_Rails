d0fd09e535596d0b3c6b6fba0e5123d92f1b18ab
ClaytonSiby <claytonsiby@gmail.com>
2022-02-03 16:02:12 +0200

    Update the new and create actions for the subjects controller.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,9 +8,21 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new
   end
 
   def create
+    # instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+
+    # try and save the subject
+    if @subject.save
+      # if the save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      # if save fails, redisplay the form to user.
+      render(:new)
+    end
   end
 
   def edit
@@ -23,5 +35,11 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :visiblle, :position)
   end
 end
